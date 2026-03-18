af8e92438db1afdccfa1a6a1b58dc00ec05d40eb
Letícia Trentin <leticiamtrentin@gmail.com>
2026-03-18 11:18:15 -0300

    corrige tema do spinner

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -51,7 +51,8 @@ const handleDataLogin = (form: User) => {
 };
 
 const submitLoginForm = async () => {
-  const { auth } = await userStorage.getUser(loginForm);
+  const auth = await userStorage.getUser(loginForm);
+  console.log("erro", auth);
   if (auth) {
     userStorage.stateUser = auth;
     router.push("/home");
