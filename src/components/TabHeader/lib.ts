413e81efbb59c758945e3cf314d3734b97dd6e83
Guilherme Grasel <guigrasel@gmail.com>
2026-05-20 10:34:54 -0300

    🔥 Remove aba agendar sala

--- src/components/TabHeader/lib.ts
+++ src/components/TabHeader/lib.ts
@@ -1,6 +1,6 @@
 export const tabItems = [
   { name: "home", label: "home" },
-  { name: "rooms", label: "bookRoom" },
+  // { name: "rooms", label: "bookRoom" },
   { name: "certifications", label: "certifications" },
   { name: "ramais", label: "extensions" },
   // { name: "rhDivulga", label: "hRdiscloses" },
