f810c3aa1cd7970795c03b3debe064a43d0a7a8f
Guilherme Moura <83136488+luismoura03@users.noreply.github.com>
2026-05-22 10:08:11 -0300

    🐛 Corrige entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -17,6 +17,7 @@ banner () {
 setup () {
   VITE_APP_ENDPOINT=${VITE_APP_ENDPOINT:-${VITE_GRAPHQL_API_URL:-}}
   VITE_URL_BACK_SERVER_EXPRESS_FOR_ARCHIVES=${VITE_URL_BACK_SERVER_EXPRESS_FOR_ARCHIVES:-${VITE_DATASOURCE:-}}
+  LAYOUT=${LAYOUT:-CIRON}
 
   escape_js_string() {
     printf "%s" "$1" | sed "s/\\\\/\\\\\\\\/g; s/'/\\\\'/g"
@@ -24,17 +25,19 @@ setup () {
 
   APP_ENDPOINT_ESCAPED=$(escape_js_string "$VITE_APP_ENDPOINT")
   DATA_SOURCE_ESCAPED=$(escape_js_string "$VITE_URL_BACK_SERVER_EXPRESS_FOR_ARCHIVES")
+  LAYOUT_ESCAPED=$(escape_js_string "$LAYOUT")
 
   cat > /usr/share/caddy/env.js <<EOF
 window.__APP_CONFIG__ = {
   VITE_APP_ENDPOINT: '$APP_ENDPOINT_ESCAPED',
-  VITE_URL_BACK_SERVER_EXPRESS_FOR_ARCHIVES: '$DATA_SOURCE_ESCAPED'
-  LAYOUT: '${LAYOUT_ESCAPED}'
+  VITE_URL_BACK_SERVER_EXPRESS_FOR_ARCHIVES: '$DATA_SOURCE_ESCAPED',
+  LAYOUT: '$LAYOUT_ESCAPED'
 };
 EOF
 
   echo -e "${VITE_APP_ENDPOINT}"
   echo -e "${VITE_URL_BACK_SERVER_EXPRESS_FOR_ARCHIVES}"
+  echo -e "${LAYOUT}"
 }
 
 httpMode () {
